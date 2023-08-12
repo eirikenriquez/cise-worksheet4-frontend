6ece34628c43473eb44998a761867d2b68697065
Eirik Enriquez <eirikdbbd@gmail.com>
2023-08-13 11:19:30 +1200

    FIXED JSON FORMAT

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps, NextPage } from "next";
 import SortableTable from "../../components/table/SortableTable";
-import data from "../../utils/dummydata";
+import data from "../../utils/dummydata.json";
 interface ArticlesInterface {
   id: string;
   title: string;
